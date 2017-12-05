7c6a9d62fd28118cd39e9f5270ba3f6b90fc6060
SnShalitha <shalitha.idm@gmail.com>
2017-12-05 15:39:24 +0530

    date 2017-12-05

--- src/DBConnect/dbConnect.java
+++ src/DBConnect/dbConnect.java
@@ -4,7 +4,7 @@ public class dbConnect {
         //public String url = "localhost";
         public String url = "172.16.20.180";
         public String port = "3306"; //8889
-        public String database = "test";
+        public String database = "hacklnjava";
         public String user = "admin";
         public String password = "stcs";//root
         public String JDBC_DRIVER = "com.mysql.jdbc.Driver";
@@ -14,17 +14,27 @@ public class dbConnect {
         public void Connect2DB()
         {
             String DB_URL = "jdbc:mysql://"+url+":"+port+"/"+database;
-            Statement dbstmt=null;
+            Statement stmnt=null;
+            DBConnect.dbConnect db=new dbConnect();
+            stmnt=db.Conn2DB();
+            //  Statement dbstmt=null;
             try {
-                conn = DriverManager.getConnection(DB_URL,user,password);//Connect
+                stmnt.executeUpdate("insert into userdetails values(1000102,'Wsta','loves wayiti','jj',3)");
+                ResultSet res=stmnt.executeQuery("select  * from userdetails ");
+
+                while(res.next())
+                    if((res.getInt("Role")==3))
+                    System.out.println(res.getString("Email")+" "+res.getString("UserName"));
+                //conn = DriverManager.getConnection(DB_URL,user,password);//Connect
                 //dbstmt = conn.createStatement();
-                System.out.println("Connected");
+               // System.out.println("Connected");
             }
             catch (SQLException se)
             {
                 se.printStackTrace();
             }
             //return dbstmt;
+            db.closeDb();
         }
 
 
@@ -42,6 +52,14 @@ public class dbConnect {
             se.printStackTrace();
         }
         return dbstmt;
+    }
+    public  void closeDb(){
+            try{
+                System.out.printf("CLOSING");
+                conn.close();
+            }catch (SQLException ex){
+                ex.getStackTrace();
+            }
     }
 
 }
